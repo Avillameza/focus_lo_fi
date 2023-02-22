d7e3cb0cdefd1eae52faca5fb96ec74cf48f23e9
Benjamin Teng <benjamin.roland.teng@gmail.com>
2023-02-21 19:33:13 -0800

    Removing test seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-playlist1 = Playlist.create(id: 'PLloVUFsFFX2y89313xIzfaRgG7EoYppyZ')
